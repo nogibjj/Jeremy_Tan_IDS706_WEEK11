e155662d364b1511a1c57bfb21ae70c65fd07568
jeremymtan <jemtan@ucsc.edu>
2023-11-09 23:07:35 -0500

    update README

--- mylib/query_viz.py
+++ mylib/query_viz.py
@@ -5,6 +5,7 @@ query and viz file
 from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
 
+
 # sample query
 def query_transform():
     """
@@ -31,38 +32,45 @@ def query_transform():
     return query_result
 
 
-
-
 # sample viz for project
 def viz():
     query = query_transform()
+    count = query.count()
+    if count > 0:
+        print(f"Data validation passed. {count} rows available.")
+    else:
+        print("No data available. Please investigate.")
     plt.figure(figsize=(15, 8))  # Adjusted figure size
-    query.select('seconds_before_next_point', 
-                 'server').toPandas().boxplot(column='seconds_before_next_point', 
-                                              by='server')
-    plt.xlabel('Server')
-    plt.ylabel('Seconds Before Next Point')
-    plt.suptitle('')
-    plt.title('Seconds Before Next Point by Server')
+    query.select("seconds_before_next_point", "server").toPandas().boxplot(
+        column="seconds_before_next_point", by="server"
+    )
+    plt.xlabel("Server")
+    plt.ylabel("Seconds Before Next Point")
+    plt.suptitle("")
+    plt.title("Seconds Before Next Point by Server")
     # Adjust the rotation and spacing of x-axis labels
-    plt.xticks(rotation=30, ha='right')  # ha='right' aligns the labels to the right
+    plt.xticks(rotation=30, ha="right")  # ha='right' aligns the labels to the right
     plt.tight_layout()  # Ensures proper spacing
-    plt.show('server.png')  
-    average_seconds_by_surface = query.groupBy('surface').avg(
-        'seconds_before_next_point')
+    plt.show("server.png")
+    average_seconds_by_surface = query.groupBy("surface").avg(
+        "seconds_before_next_point"
+    )
 
     df_surface_avg = average_seconds_by_surface.toPandas()
 
     # Create a bar chart
     plt.figure(figsize=(10, 6))
-    plt.bar(df_surface_avg['surface'], 
-            df_surface_avg['avg(seconds_before_next_point)'], 
-            color='blue')
-    plt.xlabel('Surface Type')
-    plt.ylabel('Average Seconds Before Next Point')
-    plt.title('Average Seconds Before Next Point by Surface Type')
+    plt.bar(
+        df_surface_avg["surface"],
+        df_surface_avg["avg(seconds_before_next_point)"],
+        color="blue",
+    )
+    plt.xlabel("Surface Type")
+    plt.ylabel("Average Seconds Before Next Point")
+    plt.title("Average Seconds Before Next Point by Surface Type")
     plt.xticks(rotation=45)
-    plt.show('surface.png')
+    plt.show("surface.png")
+
 
 if __name__ == "__main__":
     query_transform()
